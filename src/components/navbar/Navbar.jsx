e8a121bd2676c9272a7349b55ce1549d6d66aa77
kymo1970 <kymo1970@gmail.com>
2023-04-11 20:12:28 -0700

    Commit changes

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
+//BEM Block Element Modifier.
+const Navbar = () => {
+    return (
+        <div className='gpt3__navbar'>{/* BEM Block Element Modifier */}
+            <div className='gpt3__navbar-links'>{/* BEM Block Element Modifier */}
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./possibility.css";
+
+const Possibilty = () => {
+    return (
+        <div>Possibilty</div>
+    );
+}
+
+export default Possibilty;
+
